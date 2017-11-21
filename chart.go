44f10ed207a2f385a2861ae2efd5e62d3efab7c4
Rohan Allison <rohan.allison@cbre.com>
2017-11-21 14:57:08 -0600

    Add cCharts overlap value as pointer

--- chart.go
+++ chart.go
@@ -427,7 +427,7 @@ func (f *File) drawBarChart(formatSet *formatChart) *cPlotArea {
 	}
 	if formatSet.Type == "barStacked" {
 		c.Grouping.Val = "stacked"
-		c.Overlap.Val = 100
+		c.Overlap = &attrValInt{ Val: 100 }
 	}
 	catAx := f.drawPlotAreaCatAx()
 	valAx := f.drawPlotAreaValAx()
